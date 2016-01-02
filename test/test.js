1940a9c55ab52ee75ce1794c5e05d1bfb6fc33a4
tonykung <tonykung06@gmail.com>
2016-01-02 21:22:40 +0800

    add test - using with spread operator

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var assert = require('assert');
-var makeObjectIterable = require('../index');
+var makeObjectIterable = require('../');
 
 describe('makeObjectIterable', function() {
 	it('should make an object can be used in for...of loop', function () {
@@ -20,5 +20,18 @@ describe('makeObjectIterable', function() {
 		assert.equal(results.length, 2);
 		assert.equal(results.indexOf('value1') > -1, true);
 		assert.equal(results.indexOf('value2') > -1, true);
+	});
+
+	it('could be used with spread operator', function() {
+		const originalObj = {
+		    prop1: 'value1',
+		    prop2: 'value2'
+		};
+
+		const iterableObj = makeObjectIterable(originalObj);
+
+		assert.equal([...iterableObj].length, 2);
+		assert.equal([...iterableObj].indexOf('value1') > -1, true);
+		assert.equal([...iterableObj].indexOf('value2') > -1, true);
 	});
 });
